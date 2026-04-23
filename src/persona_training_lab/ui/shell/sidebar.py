9ca1d242a3af87cd02e4222da04f4407271609a2
quendoris <quendoris@proton.me>
2026-04-23 19:32:47 +0000

    Nudge sidebar badge and label padding right

--- src/persona_training_lab/ui/shell/sidebar.py
+++ src/persona_training_lab/ui/shell/sidebar.py
@@ -24,8 +24,8 @@ from persona_training_lab.ui.viewmodels.style import StyleViewModel
 
 SIDEBAR_ICON_RENDER_SIZE = 18
 SIDEBAR_ICON_OPTICAL_OFFSET_X = -40
-SIDEBAR_ICON_BADGE_LEFT = 6
-SIDEBAR_TEXT_LEFT_PADDING = 54
+SIDEBAR_ICON_BADGE_LEFT = 10
+SIDEBAR_TEXT_LEFT_PADDING = 58
 
 
 def _icons_root() -> Path:
