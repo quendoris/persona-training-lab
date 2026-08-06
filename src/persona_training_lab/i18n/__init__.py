ebc60a4f466731296754fa12184ed835e68f6c9c
quendoris <quendoris@proton.me>
2026-08-06 09:08:31 +0000

    feat(i18n): add localization package exports

---
+++ src/persona_training_lab/i18n/__init__.py
@@ -0,0 +1,11 @@
+from persona_training_lab.i18n.catalog import (
+    CatalogSet,
+    CatalogValidationError,
+    LocaleCatalog,
+)
+
+__all__ = [
+    "CatalogSet",
+    "CatalogValidationError",
+    "LocaleCatalog",
+]
