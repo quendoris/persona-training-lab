c8195966588382627993666a05875b5ec4c55ac3
quendoris <quendoris@proton.me>
2026-08-06 14:08:40 +0000

    feat(lineage): expose projection contracts

--- src/persona_training_lab/application/lineage/__init__.py
+++ src/persona_training_lab/application/lineage/__init__.py
@@ -1,11 +1,35 @@
 from __future__ import annotations
 
+from .projection import (
+    LineageEdge,
+    LineageEntityKind,
+    LineageNode,
+    LineageProjection,
+    LineageProjectionService,
+    LineageRelation,
+    LineageSource,
+    LineageSourceFailure,
+    LineageState,
+    UnresolvedLineageDependency,
+    lineage_node_id,
+)
 from .runtime_safety import (
     LineageResourceLinksRepositoryPort,
     LineageRuntimeSafety,
 )
 
 __all__ = [
+    "LineageEdge",
+    "LineageEntityKind",
+    "LineageNode",
+    "LineageProjection",
+    "LineageProjectionService",
+    "LineageRelation",
     "LineageResourceLinksRepositoryPort",
     "LineageRuntimeSafety",
+    "LineageSource",
+    "LineageSourceFailure",
+    "LineageState",
+    "UnresolvedLineageDependency",
+    "lineage_node_id",
 ]
