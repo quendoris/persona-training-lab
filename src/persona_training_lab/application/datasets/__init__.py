c19aef3fd57d32f47a96acc8ea1aac2ad846a8d2
quendoris <quendoris@proton.me>
2026-08-10 22:43:01 +0000

    Export dataset service error contract

--- src/persona_training_lab/application/datasets/__init__.py
+++ src/persona_training_lab/application/datasets/__init__.py
@@ -1,3 +1,9 @@
+from .errors import DatasetServiceError, DatasetServiceErrorCode
 from .service import DatasetSummary, DatasetsService
 
-__all__ = ["DatasetSummary", "DatasetsService"]
+__all__ = [
+    "DatasetServiceError",
+    "DatasetServiceErrorCode",
+    "DatasetSummary",
+    "DatasetsService",
+]
