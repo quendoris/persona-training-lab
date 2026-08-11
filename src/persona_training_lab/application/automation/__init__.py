eab64be18648de295de2e6df31d1d16dea7cbdc8
quendoris <quendoris@proton.me>
2026-08-11 19:47:33 +0000

    Export automation application contracts

--- src/persona_training_lab/application/automation/__init__.py
+++ src/persona_training_lab/application/automation/__init__.py
@@ -1,15 +1,19 @@
 from .service import (
+    AutomationDiscoveryIssue,
     AutomationInput,
     AutomationOutput,
     AutomationRecipe,
+    AutomationResourceClaim,
     AutomationRunResult,
     AutomationService,
 )
 
 __all__ = [
+    "AutomationDiscoveryIssue",
     "AutomationInput",
     "AutomationOutput",
     "AutomationRecipe",
+    "AutomationResourceClaim",
     "AutomationRunResult",
     "AutomationService",
 ]
