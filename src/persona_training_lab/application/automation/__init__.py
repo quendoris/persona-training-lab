0d0443649e58b120f401022dfc649bf00dbdf8e3
quendoris <quendoris@proton.me>
2026-08-11 19:44:38 +0000

    Add automation application package

---
+++ src/persona_training_lab/application/automation/__init__.py
@@ -0,0 +1,15 @@
+from .service import (
+    AutomationInput,
+    AutomationOutput,
+    AutomationRecipe,
+    AutomationRunResult,
+    AutomationService,
+)
+
+__all__ = [
+    "AutomationInput",
+    "AutomationOutput",
+    "AutomationRecipe",
+    "AutomationRunResult",
+    "AutomationService",
+]
